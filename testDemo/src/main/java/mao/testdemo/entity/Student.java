ea6fbed801b231796d1fe46d4a2b182ef5ed407b
mao <1296193245@qq.com>
2022-10-11 14:47:31 +0800

    encapsulation_HttpURLConnection

---
+++ testDemo/src/main/java/mao/testdemo/entity/Student.java
@@ -0,0 +1,159 @@
+package mao.testdemo.entity;
+
+/**
+ * Project name(项目名称)：封装HttpURLConnection
+ * Package(包名): mao.entity
+ * Class(类名): Student
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/10/11
+ * Time(创建时间)： 14:45
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+
+public class Student
+{
+    /**
+     * id
+     */
+    private long id;
+    /**
+     * 名字
+     */
+    private String name;
+    /**
+     * 别
+     */
+    private String sex;
+
+    /**
+     * 年龄
+     */
+    private int age;
+
+    /**
+     * Instantiates a new Student.
+     */
+    public Student()
+    {
+    }
+
+    /**
+     * Instantiates a new Student.
+     *
+     * @param id   the id
+     * @param name the name
+     * @param sex  the sex
+     * @param age  the age
+     */
+    public Student(long id, String name, String sex, int age)
+    {
+        this.id = id;
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+    }
+
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
+    public long getId()
+    {
+        return id;
+    }
+
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     * @return the id
+     */
+    public Student setId(long id)
+    {
+        this.id = id;
+        return this;
+    }
+
+    /**
+     * Gets name.
+     *
+     * @return the name
+     */
+    public String getName()
+    {
+        return name;
+    }
+
+    /**
+     * Sets name.
+     *
+     * @param name the name
+     * @return the name
+     */
+    public Student setName(String name)
+    {
+        this.name = name;
+        return this;
+    }
+
+    /**
+     * Gets sex.
+     *
+     * @return the sex
+     */
+    public String getSex()
+    {
+        return sex;
+    }
+
+    /**
+     * Sets sex.
+     *
+     * @param sex the sex
+     * @return the sex
+     */
+    public Student setSex(String sex)
+    {
+        this.sex = sex;
+        return this;
+    }
+
+    /**
+     * Gets age.
+     *
+     * @return the age
+     */
+    public int getAge()
+    {
+        return age;
+    }
+
+    /**
+     * Sets age.
+     *
+     * @param age the age
+     * @return the age
+     */
+    public Student setAge(int age)
+    {
+        this.age = age;
+        return this;
+    }
+
+    @Override
+    @SuppressWarnings("all")
+    public String toString()
+    {
+        final StringBuilder stringbuilder = new StringBuilder();
+        stringbuilder.append("id：").append(id).append('\n');
+        stringbuilder.append("name：").append(name).append('\n');
+        stringbuilder.append("sex：").append(sex).append('\n');
+        stringbuilder.append("age：").append(age).append('\n');
+        return stringbuilder.toString();
+    }
+}
